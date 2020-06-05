421505b068e2ee67d57dd7408e0145b92d78dece
ijemafe <noreply@ijemafe.com>
2020-06-05 09:41:39 -0700

    Organizing

--- main.go
+++ main.go
@@ -15,18 +15,52 @@ import (
 	registry "github.com/ijemafe/openrvs-registry"
 )
 
-var Inputs = make([]Input, 0)
 var Servers = make([]ServerInfo, 0)
 
 func main() {
-	resp, err := http.Get("http://64.225.54.237:8080/servers")
+	inputs, err := getHostPorts()
 	if err != nil {
 		log.Fatal(err)
+	}
+	var wg sync.WaitGroup
+	var lock = sync.RWMutex{}
+	for _, input := range inputs {
+		wg.Add(1)
+		go func(input Input) {
+			info, err := populateBeaconData(input)
+			if err != nil {
+				log.Println("beacon error:", err)
+				wg.Done()
+				return
+			}
+			if info.CurrentPlayers == 0 {
+				wg.Done()
+				return
+			}
+			lock.Lock()
+			Servers = append(Servers, info)
+			lock.Unlock()
+			wg.Done()
+		}(input)
+	}
+	wg.Wait()
+	b, err := json.Marshal(Servers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+}
+
+func getHostPorts() ([]Input, error) {
+	var inputs = make([]Input, 0)
+	resp, err := http.Get("http://64.225.54.237:8080/servers")
+	if err != nil {
+		return nil, err
 	}
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	lines := bytes.Split(bytes.TrimSuffix(b, []byte{'\n'}), []byte{'\n'})
 	for i := 1; i < len(lines); i++ {
@@ -38,91 +72,72 @@ func main() {
 			log.Println("atoi error:", err)
 			continue
 		}
-		Inputs = append(Inputs, Input{IP: host, Port: port})
+		inputs = append(inputs, Input{IP: host, Port: port})
 	}
-	//
-	var wg sync.WaitGroup
-	var lock = sync.RWMutex{}
-	for _, input := range Inputs {
-		wg.Add(1)
-		go func(input Input) {
-			report, err := beacon.GetServerReport(input.IP, input.Port+1000, 5*time.Second)
-			if err != nil {
-				wg.Done()
-				return
-			}
-			if report.NumPlayers == 0 {
-				wg.Done()
-				return
-			}
-			info := ServerInfo{
-				ServerName:     report.ServerName,
-				CurrentPlayers: report.NumPlayers,
-				MaxPlayers:     report.MaxPlayers,
-				IP:             report.IPAddress,
-				Port:           report.Port,
-				MapName:        report.CurrentMap,
-				GameMode:       report.CurrentMode,
-				MOTD:           report.MOTD,
-			}
-			var players = make([]Player, 0)
-			for i := 0; i < len(report.ConnectedPlayerNames); i++ {
-				var p Player
-				p.Name = report.ConnectedPlayerNames[i]
-				p.Kills = report.ConnectedPlayerKills[i]
-				p.Time = report.ConnectedPlayerTimes[i]
-				players = append(players, p)
-			}
-			info.Players = players
-			var maps = make([]Map, 0)
-			var limiter int
-			if len(report.MapRotation) >= len(report.ModeRotation) {
-				limiter = len(report.ModeRotation)
-			} else {
-				limiter = len(report.MapRotation)
-			}
-			for i := 0; i < limiter; i++ {
-				var m Map
-				m.Name = report.MapRotation[i]
-				m.Mode = report.ModeRotation[i]
-				maps = append(maps, m)
-			}
-			info.Maps = maps
-			if registry.GameTypes[info.GameMode] == "adv" {
-				var pvp PVPSettings
-				pvp.AutoTeamBalance = report.AutoTeamBalance
-				if report.CurrentMode == "RGM_BombAdvMode" {
-					pvp.BombTimer = report.BombTimer
-				}
-				pvp.FriendlyFire = report.FriendlyFire
-				pvp.RoundsPerMatch = report.RoundsPerMatch
-				pvp.TimePerRound = report.TimePerRound
-				pvp.TimeBetweenRounds = report.TimeBetweenRounds
-				info.PVPSettings = pvp
-			} else {
-				var coop CoopSettings
-				coop.AIBackup = report.AIBackup
-				coop.FriendlyFire = report.FriendlyFire
-				coop.TerroristCount = report.NumTerrorists
-				coop.RotateMapOnSuccess = report.RotateMapOnSuccess
-				coop.RoundsPerMatch = report.RoundsPerMatch
-				coop.TimePerRound = report.TimePerRound
-				coop.TimeBetweenRounds = report.TimeBetweenRounds
-				info.CoopSettings = coop
-			}
-			lock.Lock()
-			Servers = append(Servers, info)
-			lock.Unlock()
-			wg.Done()
-		}(input)
-	}
-	wg.Wait()
-	//
-	b, err = json.Marshal(Servers)
+	return inputs, nil
+}
+
+func populateBeaconData(input Input) (ServerInfo, error) {
+	report, err := beacon.GetServerReport(input.IP, input.Port+1000, 5*time.Second)
 	if err != nil {
-		log.Fatal(err)
+		return ServerInfo{}, err
 	}
-	fmt.Println(string(b))
+	info := ServerInfo{
+		ServerName:     report.ServerName,
+		CurrentPlayers: report.NumPlayers,
+		MaxPlayers:     report.MaxPlayers,
+		IP:             report.IPAddress,
+		Port:           report.Port,
+		MapName:        report.CurrentMap,
+		GameMode:       report.CurrentMode,
+		MOTD:           report.MOTD,
+	}
+	var players = make([]Player, 0)
+	for i := 0; i < len(report.ConnectedPlayerNames); i++ {
+		var p Player
+		p.Name = report.ConnectedPlayerNames[i]
+		p.Kills = report.ConnectedPlayerKills[i]
+		p.Time = report.ConnectedPlayerTimes[i]
+		players = append(players, p)
+	}
+	info.Players = players
+	var maps = make([]Map, 0)
+	var limiter int
+	if len(report.MapRotation) >= len(report.ModeRotation) {
+		limiter = len(report.ModeRotation)
+	} else {
+		limiter = len(report.MapRotation)
+	}
+	for i := 0; i < limiter; i++ {
+		var m Map
+		m.Name = report.MapRotation[i]
+		m.Mode = report.ModeRotation[i]
+		maps = append(maps, m)
+	}
+	info.Maps = maps
+	if registry.GameTypes[info.GameMode] == "adv" {
+		var pvp PVPSettings
+		pvp.AutoTeamBalance = report.AutoTeamBalance
+		if report.CurrentMode == "RGM_BombAdvMode" {
+			pvp.BombTimer = report.BombTimer
+		}
+		pvp.FriendlyFire = report.FriendlyFire
+		pvp.RoundsPerMatch = report.RoundsPerMatch
+		pvp.TimePerRound = report.TimePerRound
+		pvp.TimeBetweenRounds = report.TimeBetweenRounds
+		info.PVPSettings = pvp
+	} else {
+		var coop CoopSettings
+		coop.AIBackup = report.AIBackup
+		coop.FriendlyFire = report.FriendlyFire
+		coop.TerroristCount = report.NumTerrorists
+		coop.RotateMapOnSuccess = report.RotateMapOnSuccess
+		coop.RoundsPerMatch = report.RoundsPerMatch
+		coop.TimePerRound = report.TimePerRound
+		coop.TimeBetweenRounds = report.TimeBetweenRounds
+		info.CoopSettings = coop
+	}
+	return info, nil
 }
 
 type Input struct {
